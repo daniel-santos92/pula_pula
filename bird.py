c09afdf3001d423c766c278b4bf999baf5551422
Daniel Santos <daniel.elias.dos.santos@gmail.com>
2025-04-23 21:32:19 -0300

    suavisar salto

--- bird.py
+++ bird.py
@@ -6,30 +6,45 @@ class Bird:
         self.texture_ids = texture_ids
         self.current_frame = 0
         self.frame_time = 0
-        self.animation_speed = 0.1  # Tempo entre os frames em segundos
+        self.animation_speed = 0.1
         self.last_frame_change = time.time()
-        self.x_position = -0.6  # Mover o pássaro mais para a esquerda (de 0 a -0.6)
-        self.height = 0.5  # Começar no centro da tela
+        self.x_position = -0.6
+        self.height = 0.5
         self.speed = 0.0
-        self.width = 0.07  # Reduzido a partir de 0.1
-        self.sprite_height = 0.1  # Diminuído de 0.3
-        
-    def update(self, delta_time, gravity):
+        self.width = 0.07
+        self.sprite_height = 0.1
+
+        # Parâmetros refinados para suavidade
+        self.gravity = -2.0         # gravidade bem mais leve
+        self.jump_velocity = 0.9    # pulo mais suave
+        self.max_fall_speed = -0.8  # queda controlada
+        self.damping = 0.995        # amortecimento maior
+
+    def update(self, delta_time, _):
+        # Aplica física de queda com gravidade e amortecimento
+        self.speed += self.gravity * delta_time
+        self.speed *= self.damping  # amortecimento gradual
+
+        # Limitar velocidade máxima de queda
+        if self.speed < self.max_fall_speed:
+            self.speed = self.max_fall_speed
+
+        # Atualizar altura com base na velocidade
         self.height += self.speed * delta_time
-        self.speed += gravity * delta_time
-        
-        if self.height <= -0.4:  # Permitir que o pássaro caia até a parte inferior da tela
+
+        # Limitar a altura mínima (chão)
+        if self.height <= -0.4:
             self.height = -0.4
             self.speed = 0
-            
-        # Atualizar o quadro de animação
+
+        # Atualizar frame de animação
         current_time = time.time()
         if current_time - self.last_frame_change >= self.animation_speed:
             self.current_frame = (self.current_frame + 1) % len(self.texture_ids)
             self.last_frame_change = current_time
-            
+
     def jump(self):
-        self.speed = 1.7
+        self.speed = self.jump_velocity
 
     def draw(self):
         glEnable(GL_TEXTURE_2D)
@@ -51,9 +66,8 @@ class Bird:
         glEnd()
 
         glDisable(GL_TEXTURE_2D)
-        
+
     def get_bounding_box(self):
-        # Retornar uma tupla (esquerda, fundo, direita, topo) para detecção de colisão
         left = self.x_position - self.width
         bottom = -0.4 + self.height
         right = self.x_position + self.width
